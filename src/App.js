609a3fc55e635034b0b8041f90a8ca3a6c1e3e05
kesalkarsaket <saketkesalkar98@gmail.com>
2024-08-17 23:33:21 +0530

    sign in form and login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Body />
     </div>
   );
 }
